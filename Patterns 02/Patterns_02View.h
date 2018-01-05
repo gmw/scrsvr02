78846d178ab293786f25536402cba889858ffe61
Magnus Wissler <magwiz@icloud.com>
2018-01-05 17:27:35 +0100

    Initial Commit

---
+++ Patterns 02/Patterns_02View.h
@@ -0,0 +1,13 @@
+//
+//  Patterns_02View.h
+//  Patterns 02
+//
+//  Created by Magnus Wissler on 2018-01-05.
+//  Copyright © 2018 Magnus Wissler. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface Patterns_02View : ScreenSaverView
+
+@end
---
+++ Patterns 02/Patterns_02View.m
@@ -0,0 +1,52 @@
+//
+//  Patterns_02View.m
+//  Patterns 02
+//
+//  Created by Magnus Wissler on 2018-01-05.
+//  Copyright © 2018 Magnus Wissler. All rights reserved.
+//
+
+#import "Patterns_02View.h"
+
+@implementation Patterns_02View
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
